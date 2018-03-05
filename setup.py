81e8397da017650f4ae847f49b1023421ce1559e
YaqiangCao <caoyaqiang0410@gmail.com>
2018-03-05 21:05:40 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 
 setup(name='cLoops',
-    version='0.5',
+    version='0.6',
     author='Yaqiang Cao',
     author_email='caoyaqiang@picb.ac.cn',
     url='https://github.com/YaqiangCao/cLoops',
