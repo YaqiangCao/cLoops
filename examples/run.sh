905d3dcf3b2fde5026c21d6384cee77ff38aa9d2
YaqiangCao <caoyaqiang0410@gmail.com>
2019-05-02 09:46:10 -0400

    Update run.sh

--- examples/run.sh
+++ examples/run.sh
@@ -1,4 +1,4 @@
-cLoops -f GSM1872886_GM12878_CTCF_ChIA-PET_chr21_hg38.bedpe.gz -o chiapet -w -j -s
+cLoops -f GSM1872886_GM12878_CTCF_ChIA-PET_chr21_hg38.bedpe.gz -o chiapet -w -j -s -m 1
 jd2juice -d chiapet -o CTCF_chr21 -org hg38
 jd2washU -d chiapet -o CTCF_chr21
 jd2fingerprint -d chiapet -plot 1 -o compare -bs 2000
